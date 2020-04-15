8f21cf7979cf6d23b318f9f704f17ef6be4f4b50
Jack2313 <570055398@qq.com>
2020-04-15 13:05:21 +0800

    add conversation manager

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
-from app.chatterbot_api.get_response import bp_response
+#from app.chatterbot_api.get_response import bp_response
+from app.chatterbot_api.conversation_manager import bp_manager
 #from app.libs.login import login_manager
 #from app.libs.db import db
 #from app.view.user.helloworld import bp_hello_world
@@ -24,8 +25,8 @@ def create_app(object_name):
     app = Flask(__name__)
     app.config.from_object(object_name)
 
-    app.register_blueprint(bp_response,url_prefix='/chatterbot')
-    #app.register_blueprint(bp_hello_world)
+    #app.register_blueprint(bp_response,url_prefix='/chatterbot')
+    app.register_blueprint(bp_manager, url_prefix='/manager')
     #app.register_blueprint(bp_account)
     #app.register_blueprint(bp_info)
     #app.register_blueprint(bp_data)
